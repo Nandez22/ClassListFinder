c4b686af9673b8be791c282c802d9299b2437f08
Joe Hernandez <112195044+Nandez22@users.noreply.github.com>
2024-12-11 02:07:57 -0600

    Add project files.

---
+++ ClassListFinder/ClassListGrabber.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using PuppeteerSharp;
+using System.Text;
+using TextCopy;
+using System;
+
+namespace GetAnator {
+    public static class ClassGrabber {
+        public static async Task<string> GetList(string address, bool copy) {
+
+            await new BrowserFetcher().DownloadAsync();
+
+            using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true });
+            using var page = await browser.NewPageAsync();
+
+            await page.GoToAsync(address);
+
+            var result = await page.EvaluateExpressionAsync<string>(@"
+                let titles = []
+                let descriptions = []
+
+                let a = document.querySelectorAll('#CourseList > div > div > div.col-sm-9 > div section .panel-course .panel')
+                a.forEach(t => titles.push(t.querySelector('.panel-heading .panel-title a div span').textContent))
+                a.forEach(d => descriptions.push(d.querySelector('.panel-collapse .panel-body').textContent
+                    .replace(/\n+/g, '').replace(/\s+/g, ' ')
+                    .replace(/Credits:\s*\d+(-\d+)?/, '')
+                    .replace(/Prerequisites:.*/,'')
+                    .trim()
+                ))
+                let result = [titles,descriptions]
+                JSON.stringify(result)
+            ");
+
+            // Deserialize the result to a tuple
+            var bruple = JsonConvert.DeserializeObject<List<List<string>>>(result);
+
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 0; i < bruple[0].Count; i++) {
+                sb.Append($"{bruple[0][i]}\n{bruple[1][i]}\n\n");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ ClassListFinder/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.RegularExpressions;
+using TextCopy;
+
+namespace GetAnator {
+    public class Program {
+        public static async Task Main(string[] args) {
+            string pattern = @"^https://www\.mnsu\.edu/academics/academic-catalog/all-active-catalogs/.*#CourseList$";
+            string address = "";
+            bool copy = false;
+
+            while(true) {
+                try {
+                    Console.WriteLine("Link: ");
+                    address = Console.ReadLine();
+                    Console.Clear();
+
+                    if(!Regex.IsMatch(address, pattern)) { throw new Exception(); }
+
+                    Console.WriteLine("Copy to clipboard Y/N?");
+                    copy = Console.ReadKey().Key == ConsoleKey.Y;
+                    Console.Clear();
+
+
+                    break;
+                }
+                catch {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Link must be a valid mnsu.edu/academics/academic-catalog link ending in #CourseList");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"WORKING! :)");
+            Console.ForegroundColor = ConsoleColor.White;
+            if(copy) { Console.WriteLine("- The output will be copied to your clipboard."); }
+            string output = await ClassGrabber.GetList(address, copy);
+
+            Console.WriteLine(output);
+
+            if(copy) {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Copied!");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                ClipboardService.SetText(output);
+            }
+
+        }
+    }
+}
